bd8f56035604b87ca17195b8c07ff38acec34e91
ayal <ayalg@wix.com>
2023-02-16 16:53:49 +0200

    filter

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import './App.css';
 import { useEffect, useState } from 'react';
 
 function parseHtmlEntities(str) {
+  if (!str) {
+    return '';
+  }
   return str.replace(/&#([0-9]{1,3});/gi, function (match, numStr) {
     var num = parseInt(numStr, 10); // read num as normal number
     return String.fromCharCode(num);
@@ -12,13 +15,28 @@ const isInTime = (showInstance, timeWindow) => {
   if (!timeWindow) {
     return true;
   }
-  console.log('in time?', showInstance.date, timeWindow);
   return (Date.now() - new Date(showInstance?.date).getTime()) <= timeWindow;
+}
+
+const isInSearch = (showData, search) => {
+  if (!search) {
+    return true;
+  }
+  if (parseHtmlEntities(showData?.show?.broadcaster).match(search)) {
+    return true;
+  }
+  if (parseHtmlEntities(showData?.show?.title).match(search)) {
+    return true;
+  }
+  return showData?.data?.some((showInstance) => {
+    return `${showInstance.broadcaster} ${showInstance.subtitle ? `/ ${showInstance.subtitle} /` : `/`} ${showInstance.date_display}`.toLowerCase().match(search.toLowerCase())
+  })
 }
 
 
 function App() {
   const [all, setAll] = useState();
+  const [search, setSearch] = useState('');
   const [timeWindow, setTimeWindow] = useState(1000 * 60 * 60 * 24 * 7 * 4);
 
   useEffect(() => {
@@ -29,13 +47,19 @@ function App() {
   }, [])
 
   return (
-    <div className="App">
+    <div className="App" style={{ display: 'flex', flexDirection: 'column' }}>
       <div style={{ display: 'flex', alignItems: 'center', 'justifyContent': 'center', gap: '5px' }}>
         <a href="javascript:void(0);" onClick={() => setTimeWindow(1000 * 60 * 60 * 24 * 7 * 4 * 6)}>last 6 months</a>
         <a href="javascript:void(0);" onClick={() => setTimeWindow(1000 * 60 * 60 * 24 * 7 * 4 * 3)}>last 3 months</a>
         <a href="javascript:void(0);" onClick={() => setTimeWindow(1000 * 60 * 60 * 24 * 7 * 4)}>last month</a>
         <a href="javascript:void(0);" onClick={() => setTimeWindow(1000 * 60 * 60 * 24 * 7)}>last week</a>
         <a href="javascript:void(0);" onClick={() => setTimeWindow(undefined)}>all</a>
+      </div>
+      <div style={{ width: '100px' }}>
+        <input value={search} placeholder='filter' onChange={(e) => {
+          setTimeWindow(undefined);
+          setSearch(e.target.value)
+        }}></input>
       </div>
       {
         all?.map((showData) => {
@@ -49,18 +73,25 @@ function App() {
             return null;
           }
 
-          return <div style={{ padding: '10px' }}>
-            <div>{parseHtmlEntities(showData.show.broadcaster)}</div>
-            <div>{parseHtmlEntities(showData.show.title)}</div>
+          if (!isInSearch(showData, search)) {
+            return null;
+          }
+
+          return <div style={{ padding: '10px', flexDirection: 'column' }}>
+            <div>{parseHtmlEntities(showData?.show?.broadcaster)}</div>
+            <div>{parseHtmlEntities(showData?.show?.title)}</div>
             {
               showData?.data?.map((showInstance, i) => {
-                return isInTime(showInstance, timeWindow) ? <div style={{display:'flex', justifyContent:'center', gap:'4px'}}>
+                if (!isInSearch({ data: [showInstance] }, search)) {
+                  return null;
+                }
+                return isInTime(showInstance, timeWindow) ? <div style={{ display: 'flex', justifyContent: 'center', gap: '4px' }}>
                   <a target='_blank' rel="noreferrer" onClick={() => {
                     console.log(showData);
                   }} href={`https://www.teder.fm/archive?show=${showData.show.id}&listen=${showInstance.id}`}>
                     {`${showInstance.broadcaster} ${showInstance.subtitle ? `/ ${showInstance.subtitle} /` : `/`} ${showInstance.date_display}`}
-                    </a>
-                    <div style={{fontSize:'10px', display: 'flex', alignItems: 'center'}}>{`(${atob(showInstance.src)})`}</div>
+                  </a>
+                  <div style={{ fontSize: '10px', display: 'flex', alignItems: 'center' }}>{`(${atob(showInstance.src)})`}</div>
                 </div> : null
               })
             }
