cc21dab2683909151531772a75a111150d9c5ce4
ayal <ayalg@wix.com>
2023-02-19 12:47:00 +0200

    router and query, fix filters, debounce

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import './App.css';
-import { useEffect, useState } from 'react';
+import { memo, useCallback, useEffect, useMemo, useRef, useState } from 'react';
+import { Routes, Route, useSearchParams } from "react-router-dom";
+import * as JSURL from "jsurl";
+import debounce from 'lodash/debounce';
+
+
 
 function parseHtmlEntities(str) {
   if (!str) {
@@ -11,14 +16,14 @@ function parseHtmlEntities(str) {
   });
 }
 
-const isInTime = (showInstance, timeWindow) => {
+const _isInTime = (showInstance, timeWindow) => {
   if (!timeWindow) {
     return true;
   }
   return (Date.now() - new Date(showInstance?.date).getTime()) <= timeWindow;
 }
 
-const isInSearch = (showData, search) => {
+const isTitleInSearch = (showData, search) => {
   if (!search) {
     return true;
   }
@@ -28,16 +33,101 @@ const isInSearch = (showData, search) => {
   if (parseHtmlEntities(showData?.show?.title).match(search)) {
     return true;
   }
-  return showData?.data?.some((showInstance) => {
+}
+
+const isShowInSearchAndTime = (showData, search, timeWindow) => {
+  const showsInTime = showData.data.filter(x => _isInTime(x, timeWindow));
+  if (!showsInTime.length) {
+    console.log('no show in time');
+    return false;
+  }
+
+  if (!search) {
+    return true;
+  }
+  return showsInTime?.some((showInstance) => {
     return `${showInstance.broadcaster} ${showInstance.subtitle ? `/ ${showInstance.subtitle} /` : `/`} ${showInstance.date_display} ${atob(showInstance.src)}`.toLowerCase().match(search.toLowerCase())
   })
 }
 
+function useQueryParam(
+  key,
+  fn = x=>x,
+) {
+  let [searchParams, setSearchParams] = useSearchParams();
+  let paramValue = searchParams.get(key);
 
-function App() {
+  let setValue = (newValue, options) => {
+      let newSearchParams = new URLSearchParams(window.location.search);
+      newSearchParams.set(key, newValue);
+      setSearchParams(newSearchParams, options);
+    };
+
+  return [fn(paramValue), setValue];
+}
+
+const Results = memo(({ all, search, timeWindow }) => {
+  console.log('rendering results', search, timeWindow);
+  return <>{
+    all?.map((showData) => {
+
+      if (!isTitleInSearch(showData, search) && !isShowInSearchAndTime(showData, search, timeWindow)) {
+        console.log('title not in search');
+        return null;
+      }
+
+      if (!isShowInSearchAndTime(showData, search, timeWindow)) {
+//        console.log('show not in search / time');
+        return null;
+      }
+
+      console.log('> hasShowInTime?', parseHtmlEntities(showData?.show?.broadcaster), isShowInSearchAndTime(showData, search, timeWindow));
+
+      return <div style={{ padding: '10px', flexDirection: 'column' }}>
+        <div>{parseHtmlEntities(showData?.show?.broadcaster)}</div>
+        <div>{parseHtmlEntities(showData?.show?.title)}</div>
+        {
+          showData?.data?.map((showInstance, i) => {
+            if (!isShowInSearchAndTime({ data: [showInstance] }, search, timeWindow)) {
+              //console.log('show not in search and time...', showInstance);
+              return null;
+            }
+            return <div style={{ display: 'flex', justifyContent: 'center', gap: '4px' }}>
+              <a target='_blank' rel="noreferrer" onClick={() => {
+                console.log(showData);
+              }} href={`https://www.teder.fm/archive?show=${showData.show.id}&listen=${showInstance.id}`}>
+                {`${showInstance.broadcaster} ${showInstance.subtitle ? `/ ${showInstance.subtitle} /` : `/`} ${showInstance.date_display}`}
+              </a>
+              <div style={{ fontSize: '10px', display: 'flex', alignItems: 'center' }}>{`(${atob(showInstance.src)})`}</div>
+            </div>
+          })
+        }
+      </div>
+    })
+  }
+  </>
+}, (prev, next) => {
+  return prev.search === next.search && prev.timeWindow === next.timeWindow
+})
+
+
+function Comp() {
   const [all, setAll] = useState();
-  const [search, setSearch] = useState('');
-  const [timeWindow, setTimeWindow] = useState();
+  const [search, setSearch] = useQueryParam("filter");
+  const [inputValue, setInputValue] = useState(search);
+  const [timeWindow, setTimeWindow] = useQueryParam("timeWindow", x=>parseInt(x));
+
+  const handleSearch = useRef(debounce((value) => {
+    console.log('>>> setting search', JSON.stringify(value));
+    setSearch(value);
+  }, 1000)).current;
+
+  useEffect(() => {
+    if (inputValue !== undefined && inputValue !== null) {
+      handleSearch(inputValue);
+    }
+  }, [inputValue]);
+
 
   useEffect(() => {
     (async () => {
@@ -54,53 +144,24 @@ function App() {
         <a href="javascript:void(0);" onClick={() => setTimeWindow(1000 * 60 * 60 * 24 * 7 * 4)}>[1 month]</a>
         <a href="javascript:void(0);" onClick={() => setTimeWindow(1000 * 60 * 60 * 24 * 7 * 2)}>[2 weeks]</a>
         <a href="javascript:void(0);" onClick={() => setTimeWindow(1000 * 60 * 60 * 24 * 7)}>[1 week]</a>
-        <a href="javascript:void(0);" onClick={() => setTimeWindow(undefined)}>[all]</a>
+        <a href="javascript:void(0);" onClick={() => setTimeWindow(0)}>[all]</a>
       </div>
       <div style={{ width: '100px' }}>
-        <input value={search} placeholder='filter' onChange={(e) => {
-          setTimeWindow(undefined);
-          setSearch(e.target.value)
+        <input value={inputValue} placeholder='filter' onChange={(e) => {
+          setInputValue(e.target.value)
         }}></input>
       </div>
-      {
-        all?.map((showData) => {
+      {all?.length ?
+        <Results all={all} search={search} timeWindow={timeWindow} /> : null}
 
-          let hasShowInTime = true;
-          if (timeWindow) {
-            hasShowInTime = showData.data.some(x => isInTime(x, timeWindow));
-          }
-
-          if (!hasShowInTime) {
-            return null;
-          }
-
-          if (!isInSearch(showData, search)) {
-            return null;
-          }
-
-          return <div style={{ padding: '10px', flexDirection: 'column' }}>
-            <div>{parseHtmlEntities(showData?.show?.broadcaster)}</div>
-            <div>{parseHtmlEntities(showData?.show?.title)}</div>
-            {
-              showData?.data?.map((showInstance, i) => {
-                if (!isInSearch({ data: [showInstance] }, search)) {
-                  return null;
-                }
-                return isInTime(showInstance, timeWindow) ? <div style={{ display: 'flex', justifyContent: 'center', gap: '4px' }}>
-                  <a target='_blank' rel="noreferrer" onClick={() => {
-                    console.log(showData);
-                  }} href={`https://www.teder.fm/archive?show=${showData.show.id}&listen=${showInstance.id}`}>
-                    {`${showInstance.broadcaster} ${showInstance.subtitle ? `/ ${showInstance.subtitle} /` : `/`} ${showInstance.date_display}`}
-                  </a>
-                  <div style={{ fontSize: '10px', display: 'flex', alignItems: 'center' }}>{`(${atob(showInstance.src)})`}</div>
-                </div> : null
-              })
-            }
-          </div>
-        })
-      }
     </div>
   );
+}
+
+function App() {
+  return <Routes>
+    <Route index element={<Comp />} />
+  </Routes>
 }
 
 export default App;
